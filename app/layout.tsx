606238c7af3f3a551ed864dc7b1248b2c32162aa
Chester <1079159037@qq.com>
2025-08-20 23:21:02 +1000

    delete cv and modify name

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Chester | Personal Portfolio",
+  description: "Chester is an international student and will become a full-stack developer.",
 };
 
 export default function RootLayout({
